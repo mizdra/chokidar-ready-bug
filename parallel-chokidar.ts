410b3a2acc1359c31dd5a0e022b2cc3134cd6ed8
mizdra <pp.mizdra@gmail.com>
2025-11-20 23:44:29 +0900

    minimum reproduction

--- parallel-chokidar.ts
+++ parallel-chokidar.ts
@@ -3,11 +3,34 @@ import { mkdir, rm, writeFile } from "fs/promises";
 import { setTimeout } from "timers/promises";
 
 if (process.argv[2] === undefined) {
-  throw new Error("Please provide the number of directories to watch as a command-line argument");
+  throw new Error(
+    "Please provide the number of directories to watch as a command-line argument"
+  );
 }
 const dirCount = parseInt(process.argv[2], 10);
-
 let addEventCount = 0;
+
+// Prepare directories
+await rm("demo", { recursive: true, force: true });
+for (let i = 0; i < dirCount; i++) {
+  await mkdir(`demo/dir-${i}`, { recursive: true });
+}
+
+// Watch directories and create files
+const promises: Promise<void>[] = [];
+for (let i = 0; i < dirCount; i++) {
+  promises.push(
+    (async () => {
+      const watcher = await watchDirectory(`demo/dir-${i}`);
+      await writeFile(`demo/dir-${i}/file.txt`, "Hello, World!");
+      await setTimeout(1000); // Wait for events to be processed before closing
+      await watcher.close();
+    })()
+  );
+}
+
+await Promise.all(promises);
+console.log(`add event count: ${addEventCount}`);
 
 async function watchDirectory(dir: string) {
   const { promise, resolve } = Promise.withResolvers<void>();
@@ -23,24 +46,3 @@ async function watchDirectory(dir: string) {
   await promise;
   return watcher;
 }
-
-// Prepare directories
-await rm("demo", { recursive: true, force: true });
-for (let i = 0; i < dirCount; i++) {
-  await mkdir(`demo/dir-${i}`, { recursive: true });
-}
-
-// Watch directories and create files
-const promises: Promise<void>[] = [];
-for (let i = 0; i < dirCount; i++) {
-  promises.push((async () => {
-    await mkdir(`demo/dir-${i}`, { recursive: true });
-    const watcher = await watchDirectory(`demo/dir-${i}`);
-    await writeFile(`demo/dir-${i}/file.txt`, "Hello, World!");
-    await setTimeout(1000); // Wait for events to be processed
-    await watcher.close();
-  })());
-}
-await Promise.all(promises);
-
-console.log(`add event count: ${addEventCount}`);
